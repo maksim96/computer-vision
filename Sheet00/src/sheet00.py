520f383dd868654684c77059c74574f63c26b05b
Moritz B <buschm@cs.uni-bonn.de>
2018-10-14 13:27:06 +0200

    Added a waitKey() after every single image. Ellipses are now properly filled.

--- Sheet00/src/sheet00.py
+++ Sheet00/src/sheet00.py
@@ -9,11 +9,15 @@ if __name__ == '__main__':
     # 2a: read and display the image
     img = cv.imread(img_path)
     cv.imshow('Original Image', img)
+    cv.waitKey(0)
+    
     
     # 2b: display the intenstity image
     intensity_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('Intensity Image', intensity_img)
+    cv.waitKey(0)
 
+    
     # 2c: for loop to perform the operation
     height, width, channels = img.shape
     img_copy_c = img.copy()
@@ -22,11 +26,17 @@ if __name__ == '__main__':
             for c in range(channels):
                 img_copy_c[x,y,c] = max(0, img[x,y,c] - 0.5*intensity_img[x,y])
     cv.imshow('2c)', img_copy_c)
+    cv.waitKey(0)
+    
+    
     # 2d: one-line statement to perfom the operation above
     img_copy_d = (img - 0.5*intensity_img[:,:,np.newaxis]).clip(min=0).astype(np.uint8) #clip sets all negative values to zero
     print(img_copy_d[0,0])
     print(img_copy_c.dtype)
     cv.imshow('2d)', img_copy_d)
+    cv.waitKey(0)
+    
+    
     # 2e: Extract a random patch
     patch_size = 16
     patch = img[(height-patch_size)//2:(height+patch_size)//2, (width-patch_size)//2:(width+patch_size)//2]
@@ -38,7 +48,9 @@ if __name__ == '__main__':
 
     img[rand_y:rand_y+patch_size,rand_x:rand_x+patch_size] = patch
     cv.imshow('Patched Image', img)
-
+    cv.waitKey(0)
+    
+    
     # 2f: Draw random rectangles and ellipses
     for i in range(20):
         x = np.random.randint(0,width-1)
@@ -49,7 +61,7 @@ if __name__ == '__main__':
         if i < 10:
             img[y:y+box_height,x:x+box_width] = color
         else:
-            img = cv.ellipse(img,(x,y),(box_width//2,box_height//2),0,0,360,(0,255,0))
+            img = cv.ellipse(img,(x,y),(box_width//2,box_height//2),0,0,360,(0,255,0),thickness=-1)
             
 
     cv.imshow('Rectangles', img)
