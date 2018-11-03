c4e4410563ae3b9d4837b28765e217c65155d15a
Moritz B <buschm@cs.uni-bonn.de>
2018-11-03 03:48:55 +0100

    Exercise 3 might be finished, if ignoring an occasional error is on your mind.

--- Sheet03/src/sheet03.py
+++ Sheet03/src/sheet03.py
@@ -82,56 +82,100 @@ def myKmeans(data, k):
     :param k: number of clusters
     :return: centers and list of indices that store the cluster index for each data point
     """
+    
+    data.astype(float)
+
     centers = np.zeros((k, data.shape[1]))
     index = np.zeros(data.shape[0], dtype=int)
     clusters = [[] for i in range(k)]
+    
 
     # initialize centers using some random points from data
-    # ....
-
+    centers = data.copy()
+    np.random.shuffle(centers)
+    centers = centers[:k]
+    
     convergence = False
     iterationNo = 0
     while not convergence:
         # assign each point to the cluster of closest center
-        # ...
+        closest = np.argmin(
+            np.sqrt(((data - centers[:, np.newaxis])**2).sum(axis=2)), axis = 0
+        )
 
+        
         # update clusters' centers and check for convergence
-        # ...
+        new_centers = np.array([data[closest==i].mean(axis=0) for i in range(k)])
+        if (new_centers == centers).all():
+            convergence = True
+        else:
+            centers = new_centers
 
+        
         iterationNo += 1
         print('iterationNo = ', iterationNo)
 
+    index = closest
     return index, centers
 
 
 def task_3_a():
     print("Task 3 (a) ...")
     img = cv.imread('../images/flower.png')
-    '''
-    ...
-    your code ...
-    ...
-    '''
-
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    arr_img = img.copy()
+    arr_img = arr_img.reshape((arr_img.size,1))
+    
+    for i in [2,4,6]:
+        index, _ = myKmeans(arr_img, i)
+        for k in range(i):
+            img[index.reshape(img.shape) == k] = k/i * 255
+        cv.imshow("k={} clustering".format(i), img)
+        cv.waitKey(0)
+    cv.destroyAllWindows()
 
 def task_3_b():
     print("Task 3 (b) ...")
     img = cv.imread('../images/flower.png')
-    '''
-    ...
-    your code ...
-    ...
-    '''
+    arr_img = img.copy()
+    arr_img = arr_img.reshape((int(arr_img.size/3),3))
+    
+    for i in [2,4,6]:
+        index, _ = myKmeans(arr_img, i)
+        for k in range(i):
+            img[index.reshape(img.shape[:-1 ]) == k] = k/(i-1) * 255
+        cv.imshow("k={} clustering".format(i), img)
+        cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 def task_3_c():
     print("Task 3 (c) ...")
     img = cv.imread('../images/flower.png')
-    '''
-    ...
-    your code ...
-    ...
-    '''
+    arr_img = img.copy()
+    arr_img = arr_img.reshape((int(arr_img.size/3),3))
+    
+    ind = np.indices(img.shape[:-1])
+    ind = np.dstack((ind[0], ind[1]))
+    print(img.shape, arr_img.shape, ind.shape)
+    ind = ind.reshape((int(arr_img.size/3), 2))
+
+    arr_img = np.concatenate((arr_img, ind), axis=1)
+
+    # todo: add relative image location scaled to 255
+
+    # normalize the data to a scale from 0 to 255
+    arr_img = arr_img.astype(float)
+    arr_img[:,-1] = (arr_img[:,-1]/arr_img[:,-1].argmax(0)) * 255
+    arr_img[:,-2] = (arr_img[:,-2]/arr_img[:,-2].argmax(0)) * 255
+
+    for i in [2,4,6]:
+        index, _ = myKmeans(arr_img, i)
+        for k in range(i):
+            img[index.reshape(img.shape[:-1 ]) == k] = k/(i-1) * 255
+        cv.imshow("k={} clustering".format(i), img)
+        cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 ##############################################
